3dc2d41e21b41231825577bd81a82279d11ea18b
maxbo <maxbo@LAPTOP-GLJ28FNK>
2018-10-05 04:17:39 -0400

    yes

--- letsgo/src/letsgo/uhhuh.java
+++ letsgo/src/letsgo/uhhuh.java
@@ -2,7 +2,7 @@ package letsgo;
 
 public class uhhuh {
 	public static void main(String[] args){
-		System.out.println("i hope this works.");
+		System.out.println("i hope this works. hwo i do this");
 	}
 
 }
